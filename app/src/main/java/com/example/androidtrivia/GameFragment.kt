42dee48b22e3e36d415b495c2c8e69badfc218d6
ditramadia <ditrarizqaamadia@gmail.com>
2024-02-09 19:41:20 +0700

    feat: add game layout

--- app/src/main/java/com/example/androidtrivia/GameFragment.kt
+++ app/src/main/java/com/example/androidtrivia/GameFragment.kt
@@ -6,13 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class TitleFragment : Fragment() {
+class GameFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_title, container, false)
+        return inflater.inflate(R.layout.fragment_game, container, false)
     }
 }
