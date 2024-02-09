d1e7171580bd02835eb8fa2d91ba9ba97a5d9517
ditramadia <ditrarizqaamadia@gmail.com>
2024-02-09 23:52:59 +0700

    feat: add up button action

--- app/src/main/java/com/example/androidtrivia/MainActivity.kt
+++ app/src/main/java/com/example/androidtrivia/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.androidtrivia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.androidtrivia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navController = this.findNavController(R.id.navHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.navHostFragment)
+        return navController.navigateUp()
     }
 }
