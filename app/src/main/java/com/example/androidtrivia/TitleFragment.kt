3dbfffc03d619e2c455842be4312c675ecaa13ab
ditramadia <ditrarizqaamadia@gmail.com>
2024-02-10 00:34:30 +0700

    feat: add about navigation menu

--- app/src/main/java/com/example/androidtrivia/TitleFragment.kt
+++ app/src/main/java/com/example/androidtrivia/TitleFragment.kt
@@ -3,10 +3,15 @@ package com.example.androidtrivia
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.androidtrivia.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -19,7 +24,16 @@ class TitleFragment : Fragment() {
         binding.playButton.setOnClickListener (
             Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
         )
-
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController()) || super.onOptionsItemSelected(item)
     }
 }
