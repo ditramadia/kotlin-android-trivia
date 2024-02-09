da33a263e49cef1a6104cb570ab2463a26939e49
ditramadia <ditrarizqaamadia@gmail.com>
2024-02-09 18:48:07 +0700

    feat: add title fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,10 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    dataBinding {
+        enable = true
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
